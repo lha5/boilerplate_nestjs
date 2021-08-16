7e760326010fd6fb368d0d636dbf1ee967d63bec
Hyeahn Lee <lha@Hyeahns-MacBook-Pro.local>
2021-08-16 17:55:02 +0900

    board model 파일 생성성

--- src/boards/boards.service.ts
+++ src/boards/boards.service.ts
@@ -1,10 +1,11 @@
 import { Injectable } from '@nestjs/common';
+import { Board } from './boards.model';
 
 @Injectable()
 export class BoardsService {
-  private boards = [];  // private를 사용하면 다른 componenets에서 값을 수정하는 것을 막을 수 있음
+  private boards: Board[] = [];  // private를 사용하면 다른 componenets에서 값을 수정하는 것을 막을 수 있음
 
-  getAllBoards() {
+  getAllBoards(): Board[] {
     return this.boards;
   }
 }
