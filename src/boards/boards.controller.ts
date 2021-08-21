b629ad8ad0a4c50641cbbc594381624feea2e45b
Hyeahn Lee <lha@Hyeahns-MacBook-Pro.local>
2021-08-21 15:41:40 +0900

    CRUD 기능 완성

--- src/boards/boards.controller.ts
+++ src/boards/boards.controller.ts
@@ -1,7 +1,7 @@
 import { CreateBoardDto } from './dto/create-board.dto';
-import { Body, Controller, Get, Post } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Param, Patch, Post } from '@nestjs/common';
 import { BoardsService } from './boards.service';
-import { Board } from './boards.model';
+import { Board, BoardStatus } from './boards.model';
 
 @Controller('boards')
 export class BoardsController {
@@ -13,7 +13,25 @@ export class BoardsController {
   }
 
   @Post('/')
-  createBoard(@Body() CreateBoardDto: CreateBoardDto): Board {
-    return this.boardsService.createBoard(CreateBoardDto);
+  createBoard(@Body() createBoardDto: CreateBoardDto): Board {
+    return this.boardsService.createBoard(createBoardDto);
+  }
+
+  @Get('/:id')
+  getBoardById(@Param('id') id: string): Board {
+    return this.boardsService.getBoardById(id);
+  }
+
+  @Delete('/:id')
+  deleteBoard(@Param('id') id: string): void {
+    this.boardsService.deleteBoard(id);
+  }
+
+  @Patch('/:id/status')
+  updateBoardStatus(
+    @Param('id') id: string,
+    @Body('status') status: BoardStatus,
+  ) {
+    return this.boardsService.updateBoardStatus(id, status);
   }
 }
