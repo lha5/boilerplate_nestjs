72a8c6f4fb7b4c149bae9d376c557befeb918c43
Hyeahn Lee <gbvchk42@gmail.com>
2021-08-21 16:24:56 +0900

    custom pipe 생성 및 실습

--- src/boards/boards.controller.ts
+++ src/boards/boards.controller.ts
@@ -2,6 +2,7 @@ import { CreateBoardDto } from './dto/create-board.dto';
 import { Body, Controller, Delete, Get, Param, Patch, Post, UsePipes, ValidationPipe } from '@nestjs/common';
 import { BoardsService } from './boards.service';
 import { Board, BoardStatus } from './boards.model';
+import { BoardStatusValidationPipe } from './pipes/board-status-validation.pipe';
 
 @Controller('boards')
 export class BoardsController {
@@ -31,7 +32,7 @@ export class BoardsController {
   @Patch('/:id/status')
   updateBoardStatus(
     @Param('id') id: string,
-    @Body('status') status: BoardStatus,
+    @Body('status', BoardStatusValidationPipe) status: BoardStatus,
   ) {
     return this.boardsService.updateBoardStatus(id, status);
   }
